357da4bdf11cbdb7d374b45f4f65d0525cf698dd
Mensurui <mensurk3000@gmail.com>
2023-07-01 11:41:51 +0300

    Books-shelf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
     <div className="App">
+    <BrowserRouter>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Link className="site-logo" to='/'>Author</Link>
+        <nav>
+        <Link to='/about'>About</Link>
+        </nav>
+        </header>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+        </Routes>
+        </BrowserRouter>
+      
     </div>
   );
 }
-
 export default App;
